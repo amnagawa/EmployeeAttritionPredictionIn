be38815b22c76d772b31d6f9cbd6bf82b0c307a5
Amol Nagawade <amol.nagawade@capgemini.com>
2018-09-24 17:21:46 +0000

    Updated Application.java

--- src/main/java/myapp/Application.java
+++ src/main/java/myapp/Application.java
@@ -1,16 +1,19 @@
-package myapp;
+package com.attrition.prediction;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.filter.CorsFilter;
+
+import com.attrition.prediction.configuration.AppConfig;
+import com.attrition.prediction.configuration.AppInitializer;
+import com.attrition.prediction.configuration.HibernateConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
 	
 	@Override
@@ -22,19 +25,19 @@ public class Application extends SpringBootServletInitializer {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-        };
-    }
+//    @Bean
+//    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+//        return args -> {
+//
+//            System.out.println("Let's inspect the beans provided by Spring Boot:");
+//
+//            String[] beanNames = ctx.getBeanDefinitionNames();
+//            Arrays.sort(beanNames);
+//            for (String beanName : beanNames) {
+//                System.out.println(beanName);
+//            }
+//
+//        };
+//    }
 
 }
