b1a8193f2fcf145f031f75dd052553683a5b7fda
Amol Nagawade <amol.nagawade@capgemini.com>
2018-09-25 15:35:39 +0000

    Added 2 files to security

---
+++ src/main/java/com/attrition/prediction/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.attrition.prediction.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
